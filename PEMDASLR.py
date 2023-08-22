086115acfff30a074c9119e05153014567bed0f5
Shubham <58857424+shivamtomershiv@users.noreply.github.com>
2023-08-22 10:54:02 +0530

    Add files via upload

---
+++ PEMDASLR.py
@@ -0,0 +1,32 @@
+number_one = input("Type your first number\n")
+number_two = input("Type your second number\n")
+
+n1 = int(number_one)
+n2 = int(number_two)
+# Addition
+sum_result = n1 + n2
+print("Sum:", sum_result)
+
+# Subtraction
+sub_result = n1 - n2
+print("Difference:", sub_result)
+
+# Multiplication
+mul_result = n1 * n2
+print("Product:", mul_result)
+
+# Division
+div_result = n1 / n2
+print("Quotient:", div_result)
+
+# Floor Division
+floor_div_result = n1 // n2
+print("Floor Quotient:", floor_div_result)
+
+# Modulo (Remainder)
+mod_result = n1 % n2
+print("Remainder:", mod_result)
+
+# Exponentiation
+exp_result = n1 ** n2
+print("Exponentiation:", exp_result)
