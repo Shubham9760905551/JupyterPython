07157ab2b790e55e599e84d987bf844e0ac41491
Shubham <58857424+shivamtomershiv@users.noreply.github.com>
2024-05-23 22:48:57 +0530

    Add files via upload

---
+++ Python Basics Programs/Daily Projects/PATD_01.py
@@ -0,0 +1,8 @@
+# Brand name Generator
+print("Welcome to the band name generator")
+# Ask the user for the city that they grew up in.
+city = input("Which city did you grow up in?\n")
+# Ask the user for name of the pet.
+pet = input("What is your pet name?\n")
+# Combine the name of their city or pet and show them their brand name.
+print("Your brand name could be "+ city + " " + pet)
